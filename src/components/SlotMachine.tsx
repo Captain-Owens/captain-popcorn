0a7386900726f2aa41956dd8b5205468e93925ef
Evan Owens <evanowens@Evans-MacBook-Air.local>
2026-03-03 18:17:21 -0800

    Fix spinner build error

--- src/components/SlotMachine.tsx
+++ src/components/SlotMachine.tsx
@@ -140,7 +140,7 @@ export default function SlotMachine({ isOpen, onClose, memberId, onWatch }: Slot
               <motion.div
                 className="flex flex-col gap-4"
                 initial={{ y: 0 }}
-                animate={{ y: -((reelPosters.length - 1) * (posterHeight + gap)) }}
+                animate={{ y: -((reelPosters.length - 1) * (posterHeight + 16)) }}
                 transition={{
                   duration: SPIN_DURATION,
                   ease: [0.2, 0.8, 0.3, 1],
